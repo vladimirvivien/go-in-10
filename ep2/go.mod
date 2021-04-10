aeb3ad5c890cc44464883c86e505c5eec25522fe
Vladimir Vivien <vivienv@vmware.com>
2021-04-10 17:29:10 -0400

    ep2 example

---
+++ ep2/go.mod
@@ -0,0 +1,3 @@
+module vladimirvivien/go-in-10
+
+go 1.16
---
+++ ep2/hello.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Vladimir"
+	fmt.Println(name)
+	fmt.Printf("Hello, %s!\n", name)
+	fmt.Println("Welcome to Go in 10 minutes!")
+}
